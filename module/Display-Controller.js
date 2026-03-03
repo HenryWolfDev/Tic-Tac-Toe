8fb9d872efe24afbd3fc5b1bc9bcd95ee0026d53
Henry <HWolf.binz@gmail.com>
2026-03-03 18:57:50 +0100

    refactor(display-controller): change module with init function to call Game.init with Player names

--- module/Display-Controller.js
+++ module/Display-Controller.js
@@ -1,4 +1,26 @@
 import { GameBoard } from './Game-Board.js';
 import { Game } from './Game.js';
 
-export const DisplayController = () => {};
+export const DisplayController = (function () {
+  const init = () => {
+    const formular = document.getElementById('main-menu-form');
+    const mainMenu = document.getElementById('main-menu-section');
+    const gameBoard = document.getElementById('gameboard-section');
+
+    formular.addEventListener('submit', event => {
+      event.preventDefault();
+      const { 'player-one': pOneName, 'player-two': pTwoName } =
+        Object.fromEntries(new FormData(event.target));
+      Game.init(pOneName, pTwoName);
+
+      mainMenu.classList.add('hidden');
+      gameBoard.classList.remove('hidden');
+    });
+  };
+
+  return {
+    init,
+  };
+})();
+
+DisplayController.init();
